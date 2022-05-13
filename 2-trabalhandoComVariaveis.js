f519858dcc76b13ae8a7e671140fd1dc2c8f7150
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-12 23:41:54 -0300

    Update 2-trabalhandoComVariaveis.js
    alteração para incluir exemplos

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -6,6 +6,7 @@ console.log("Trabalhando com variáveis");
 
 const idade = 29;
 const nome = "Ricardo";
-let ano = 2020;
+//let ano = 2020;
 
-console.log(idade);
+//console.log(idade);
+//mes = "Janeiro";
