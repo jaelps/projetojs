0e668d9c42cea0ca9793c9057b404c5bb7e26382
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-13 00:27:26 -0300

    Create 5-atribuicaoVariaveis.js
    exemplo de atribuição de variavel explicação rapida sobre const e let

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com Atribuição de Variaveis");
+
+const idade = 29;
+const primeiroNome = "Ricardo"; //const não aletra / let variavel que pode ser altrada
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nome);
