0ce165257660c3356bbe5f7f5e2c99a08c8125eb
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-12 23:38:05 -0300

    Update 2-trabalhandoComVariaveis.js
    exemplo de variáveis

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -1,7 +1,11 @@
 console.log("Trabalhando com variáveis");
 //JS é case sensitive
 
-idade = 29;
-Idade = 30;
+//idade = 29;
+//Idade = 30;
+
+const idade = 29;
+const nome = "Ricardo";
+let ano = 2020;
 
 console.log(idade);
