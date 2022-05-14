637153eb357f0ae8a37b3dd5eb667e92f82a45d7
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-14 10:43:15 -0300

    Update 7-condicionais.js
    exercicos

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,24 +6,39 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 14; 
-const estaAcompanhada = true;
+const idadeComprador = 20;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 console.log("Destinos possíveis");
 
 console.log(listaDeDestinos);
 
 //condição
-if (idadeComprador >= 18){
+// if (idadeComprador >= 18){
 
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);//removendo item
-}else if(estaAcompanhada){
-        console.log("Comprador esta acompanhado");
-        listaDeDestinos.splice(1,1);
-    }else{
-        console.log("Menor idade não altorizado venda");
-    }
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1,1);//removendo item
+// }else if(estaAcompanhada){
+//         console.log("Comprador esta acompanhado");
+//         listaDeDestinos.splice(1,1);
+//     }else{
+//         console.log("Menor idade não altorizado venda");
+//     }
 
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem");
+    listaDeDestinos.splice(2, 1);//removendo item
+} else {
+    console.log("Menor idade não altorizado venda");
+}
+
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa Viagem");
+}else{
+    console.log("Você não pode embarcar");
+}
 
 console.log(listaDeDestinos);
 
