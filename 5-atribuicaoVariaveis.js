e6a37509399a8e8ffdb4258a4d80d70fa1172202
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-14 08:24:26 -0300

    Update 5-atribuicaoVariaveis.js
    Correção de erro sintaxe necessária para codigo, adicionado novo identificador "primeiroNome", para melhor entendimento

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,12 +1,17 @@
 console.log("Trabalhando com Atribuição de Variaveis");
 
-const idade = 29;
+//const idade = 29;
 const primeiroNome = "Ricardo"; //const não aletra / let variavel que pode ser altrada
 const sobrenome = "Bugan";
 
 //console.log(nome + " " + sobrenome);
-console.log(nome, sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-const nomeCompleto = nome + sobrenome;
-console.log(nome);
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade;
+idade = 26;
+idade = idade +1;
+console.log(idade);
