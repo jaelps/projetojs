2bedef070e73ef16550d960dcf1d66048c12374f
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-14 08:44:50 -0300

    Create 6-listas.js
    Estudos sobre utilização listas

---
+++ 6-listas.js
@@ -0,0 +1,16 @@
+console.log("Trabalhado com listas");
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`); //adicionado item
+console.log("Destinos possíveis");
+//console.log(salvador, saoPaulo, rioDeJaneiro);
+console.log(listaDeDestinos);
